fd8ac9ee18e1100377b1a2ed52c436bfe1df2a08
jonassantoss <jonassoares.live@gmail.com>
2024-06-05 18:41:13 -0300

    Add new pages

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,7 +1,51 @@
+const dayjs = require('dayjs')
+const relativeTime = require('dayjs/plugin/relativeTime')
+require('dayjs/locale/pt-br')
+dayjs.extend(relativeTime);
+dayjs.locale('pt-br')
+
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient()
+
 const Produto = require("../models/productModel");
 
-exports.index = (req, res) => {
-  res.render("produto", {
+exports.index = async (req, res) => {
+  const { page = 1, search = '' } = req.query;
+  const limit = 10;
+  const skip = (page - 1) * limit;
+
+  const query = search ? { name: { contains: search } } : {}
+
+  try {
+      const products = await prisma.products.findMany({
+          where: query,
+          skip: skip,
+          take: limit,
+          orderBy: { createdAt: 'desc' }
+      })
+      const count = await prisma.products.count({ where: query })
+
+      const productsFormatted = products.map(product => {
+          product.createdAt = dayjs().to(product.createdAt.toLocaleString())
+          product.updatedAt = dayjs().to(product.updatedAt.toLocaleString())
+          return product
+      })
+
+    res.render("produto", {
+      product: {},
+      products: productsFormatted,
+      currentPage: parseInt(page),
+      totalPages: Math.ceil(count / limit),
+      search
+    })
+  } catch (error) {
+    console.error(error)
+    res.status(500).send('Internal Server Error')
+  }
+}
+
+exports.indexNovo = (req, res) => {
+  res.render("novoProduto", {
     product: {},
   });
 };
@@ -13,13 +57,13 @@ exports.register = async (req, res) => {
 
     if (product.errors.length > 0) {
       req.flash("errors", product.errors);
-      req.session.save(() => res.redirect(`/produto/index`));
+      req.session.save(() => res.redirect(`/produto/novo`));
       return;
     }
 
     req.flash("success", "Produto registrado com sucesso");
     req.session.save(() =>
-      res.redirect(`/`)
+      res.redirect(`/produto`)
     );
     return;
   } catch (error) {
@@ -49,7 +93,7 @@ exports.edit = async (req, res) => {
       return
     } else {
       req.flash("success", "Produto editado com sucesso!")
-      req.session.save(() => res.redirect('/'))
+      req.session.save(() => res.redirect('/produto'))
       return
     }
   } catch (error) {
@@ -64,7 +108,7 @@ exports.delete = async (req, res) => {
   try {
     await Produto.delete(req.params.id)
     req.flash('success', 'Produto excluido com sucesso!')
-    req.session.save(() => res.redirect('/'))
+    req.session.save(() => res.redirect('/produto'))
   } catch (error) {
     console.log(error)
     res.rend('404')
